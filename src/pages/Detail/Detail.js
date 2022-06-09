25b29d8545778baca5a042fe8e03738f4e36a7d6
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-06-09 20:32:31 +0300

    initial commit

---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import styles from './Detail.style';
+
+const Detail = () => {
+  return (
+    <View>
+      <Text>Detail</Text>
+    </View>
+  );
+};
+
+export default Detail;
