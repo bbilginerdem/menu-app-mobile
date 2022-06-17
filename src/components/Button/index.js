23e4428e15751affd6db6a7bb28b8d3b52badd52
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-06-17 21:54:50 +0300

    unit test integration

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import {TouchableOpacity, View, Text} from 'react-native';
+import React from 'react';
+
+const Button = () => {
+  return (
+    <TouchableOpacity>
+      <Text>Button</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
