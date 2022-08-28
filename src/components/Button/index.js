ca3a2227aee9600c18e8580318c3bf8380756424
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-08-28 23:27:16 +0300

    button added

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,1 @@
-import {TouchableOpacity, View, Text} from 'react-native';
-import React from 'react';
-
-const Button = () => {
-  return (
-    <TouchableOpacity>
-      <Text>Button</Text>
-    </TouchableOpacity>
-  );
-};
-
-export default Button;
+export {default} from './Button';
